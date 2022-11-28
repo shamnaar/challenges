3531e48ff364353f4e2ec9e218616bb3956a8e88
Ar, Shamna <shamna.ar@rakuten.com>
2022-11-28 18:40:16 +0530

    challenge3

--- object.py
+++ object.py
@@ -2,8 +2,10 @@ def myfunction(object, key):
     for key in key.split('/'):
         object = object[key]
     return object
+
 #object = {"a":{"b":{"c":"d"}}}
 #key = 'a/b/c'
+
 object = {"x":{"y":{"z":"a"}}}
 key = 'x/y/z'
 
